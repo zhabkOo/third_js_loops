e4844e8b5c5256df3982d3381611358d5d024ba8
zhabkOo <freem.ua@gmail.com>
2020-02-08 17:08:31 +0200

    If 'E' or 'w' check

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 //--------------------Случайная строка. Использовать в решении цикл for или while----------------//
 let randIndex = "";
 let randChar = "";
+let randCharStr = "";
 for (let i = 0; i < 26; i++) {
   randIndex = Math.floor(Math.random() * (122 - 65 + 1)) + 65;
-  randChar += String.fromCharCode(randIndex);
+  randChar = String.fromCharCode(randIndex);
+  randCharStr += String.fromCharCode(randIndex);
+  if (randChar === "E" || randChar === "w") {
+    break;
+  }
 }
+console.log(randIndex);
 console.log(randChar);
+console.log(randCharStr);
